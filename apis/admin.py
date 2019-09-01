9e8b19b14cbd36f6eeff6d3362e80166252d90eb
Sanjampreet Singh <sanjam531@gmail.com>
2019-09-01 11:18:32 +0530

    API Admin

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (Api, ApiCategory, ApiImage)
 
-# Register your models here.
+
+class ApiAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ('name', 'category', 'trial',
+                    'active', 'recommendations', 'price')
+
+    class Meta:
+        model = Api
+
+
+admin.site.register(Api, ApiAdmin)
+admin.site.register(ApiCategory)
+admin.site.register(ApiImage)
