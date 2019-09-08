82252ecbe5b2a1f9bfc9bf9caee759cd8a0a99e1
Sanjampreet Singh <sanjam531@gmail.com>
2019-09-08 11:10:44 +0530

    ModuleAdmin

--- modules/admin.py
+++ modules/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (Modules)
 
-# Register your models here.
+
+class ModuleAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ('name', 'active')
+
+    class Meta:
+        model = Modules
+
+
+admin.site.register(Modules, ModuleAdmin)
