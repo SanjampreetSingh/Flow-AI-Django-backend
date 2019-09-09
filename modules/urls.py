5f77297aaa5fbcf9856f8c3fc6f6579513173985
Sanjampreet Singh <sanjam531@gmail.com>
2019-09-09 12:33:13 +0530

    ModuleDetails

--- modules/urls.py
+++ modules/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('module/', views.ModuleList.as_view(),
          name="module_list"),
+    re_path(r'^products/(?P<pk>\d+)$',
+            views.ModuleDetails.as_view(), name="module_details"),
 ]
--- modules/views.py
+++ modules/views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 # Django
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
@@ -9,19 +7,25 @@ from django.http import HttpResponse, Http404
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveAPIView
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 # Django Rest Framework JWT
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 # Local
-from users.models import (Users)
 from .models import (Modules)
 from .serializer import (ModuleSerializer)
 
 
 # Api Category List
 class ModuleList(ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_class = (JSONWebTokenAuthentication,)
+    queryset = Modules.objects.all()
+    serializer_class = ModuleSerializer
+
+
+class ModuleDetails(RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
     authentication_class = (JSONWebTokenAuthentication,)
     queryset = Modules.objects.all()
