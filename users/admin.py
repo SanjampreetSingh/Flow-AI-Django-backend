f402e0e958fd0222f3fe141a0d2f55ea9780a2aa
Sanjampreet Singh <sanjam531@gmail.com>
2019-09-25 15:54:21 +0530

    admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.template.response import TemplateResponse
 from django.urls import path
