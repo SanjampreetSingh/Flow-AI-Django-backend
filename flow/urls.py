5dd900207fe882f245c3404f2c335d81f73480b8
Sanjampreet Singh <sanjam531@gmail.com>
2019-09-25 18:20:29 +0530

    redirect to other url

--- flow/urls.py
+++ flow/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', RedirectView.as_view(url='https://theflowai.com')),
     path('api/', include([
         path('auth/', include('rest_framework_social_oauth2.urls')),
         path('', include('comman.urls')),
