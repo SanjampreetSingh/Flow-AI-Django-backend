a3a5b02a6bbf1537f7f25b9661e0ac0ab35e22eb
Sanjampreet Singh <sanjam531@gmail.com>
2019-09-28 19:09:29 +0530

    setting up production db

--- flow/settings/production.py
+++ flow/settings/production.py
@@ -3,3 +3,15 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['167.71.235.193', 'api.theflowai.com']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'flow-app',
+        'USER': 'flow_admin',
+        'PASSWORD': 'yxwr24BCqbh3bfM',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
