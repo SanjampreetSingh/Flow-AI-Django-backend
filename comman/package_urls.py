7db719662c52da7f9ac834cf06e90532e5fbca42
Sanjampreet Singh <sanjam531@gmail.com>
2019-09-29 09:17:30 +0530

    code for package

--- comman/package_urls.py
+++ comman/package_urls.py
@@ -4,6 +4,8 @@ from rest_framework.routers import DefaultRouter
 # From Package VIEW
 from package.views import(
     packageReadyApiCallInference,
+    validateApiKey,
+    activeApiList
 )
 
 
@@ -12,5 +14,9 @@ urlpatterns = [
     path('package/', include([
         # Ready Api Call Inference From Package VIEW
         path('call/ready/', packageReadyApiCallInference, name='ready_api_call'),
+        # Validate API KEY From Package VIEW
+        path('check/api-key/', validateApiKey, name='validate_api_key'),
+        # Get List of Activate APIs From Package VIEW
+        path('get/models/', activeApiList, name='active_api_list'),
     ])),
 ]
